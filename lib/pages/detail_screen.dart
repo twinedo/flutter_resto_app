9e1ba608986961e3fe62038e64411d117fdbce2a
twinedo <twinedo@github.com>
2021-10-25 17:15:27 +0700

    add detail page

--- lib/pages/detail_screen.dart
+++ lib/pages/detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_app_sub1/models/resto_model.dart';
+import 'package:restaurant_app_sub1/theme.dart';
 
 class DetailScreen extends StatelessWidget {
   final RestoModel resto;
@@ -7,8 +8,150 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(resto.name.toString()),
+    var screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 50,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.chevron_left,
+                            color: Colors.black,
+                          ))),
+                  Flexible(
+                    child: Container(
+                      child: Text(
+                        resto.name.toString(),
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                  CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.favorite_outline,
+                            color: Colors.black,
+                          )))
+                ],
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Card(
+                      elevation: 20.0,
+                      color: Colors.white.withOpacity(0),
+                      child: Container(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(25.0),
+                          child: Image.network(
+                            resto.pictureId.toString(),
+                            width: screenWidth * 0.9,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(20),
+                      width: screenWidth,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            child: IntrinsicHeight(
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        Icons.pin_drop,
+                                        color: Colors.green,
+                                      ),
+                                      Text(
+                                        resto.city.toString(),
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                            color: darkColor),
+                                      )
+                                    ],
+                                  ),
+                                  VerticalDivider(
+                                    width: 10,
+                                    color: Colors.grey,
+                                    thickness: 1,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        Icons.star,
+                                        color: Colors.yellow,
+                                      ),
+                                      Text(resto.rating.toString(),
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                              color: darkColor))
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'Description',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: darkColor,
+                                fontSize: 20),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              resto.description.toString(),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ),
+                          Text(
+                            'Menus',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: blackColor,
+                                fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -9,6 +9,7 @@ Color blackColor = Color(0xff272C2F);
 Color whiteColor = Color(0xffFFFFFF);
 Color redColor = Color(0xffFD4F56);
 Color greyColor = Color(0xff747987);
+Color darkColor = Color(0xff444444);
 Color transparentColor = Colors.transparent;
 Color inputFieldColor = Color(0xffF1F0F5);
 Color backgroundColor = Color(0xffF5F2EE);
